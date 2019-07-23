c92dce6b27a57e1c191aa18e5ff520eeb868d3c5
Seth Comrie <9946365@student.toiohomai.ac.nz>
2019-07-24 11:27:27 +1200

    Today I learnt about Hello World

---
+++ FirstTask/FirstTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your last name");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Hello {0} {1}, nice to meet you.", name, lname);
+            Console.ReadLine();
+            
+        }
+    }
+}
